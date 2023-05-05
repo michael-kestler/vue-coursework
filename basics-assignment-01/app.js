154bc1bab9792b6be0a61354919449f96dcc129e
michael-kestler <michael.s.kestler@gmail.com>
2023-05-05 16:47:21 -0400

    Added random number to para with vue interpolation

--- basics-assignment-01/app.js
+++ basics-assignment-01/app.js
@@ -3,9 +3,10 @@ const app = Vue.createApp({
         return{
             yourName: 'Michael Kestler',
             yourAge: 'Age: 31',
-            ageInFive: 'Age in 5 years: 36'
+            ageInFive: 'Age in 5 years: 36',
+            randomNumber: 'Favorite Number: ' + (Math.random()).toFixed(2)
+        };
 
-        }
     }
 });
 
