9b95070cb2465eedcbe423328efe0872478942c4
michael-kestler <michael.s.kestler@gmail.com>
2023-05-09 17:22:27 -0400

    added value to html using vue method

--- basics-assignment-01/app.js
+++ basics-assignment-01/app.js
@@ -7,7 +7,16 @@ const app = Vue.createApp({
             randomNumber: 'Favorite Number: ' + (Math.random()).toFixed(2),
             imageUrl: 'https://media.licdn.com/dms/image/C4D03AQGfl52yFeHRow/profile-displayphoto-shrink_800_800/0/1517448638219?e=1689206400&v=beta&t=Hf2plOaVJuNtHRpbbzCpMO-XhV8ry4qh7w0i2nYrru0'
         };
-
+    },
+    methods: {
+        randomNumMethod(){
+            const rando = (Math.random()).toFixed(2);
+            if (rando === .14){
+                return rando + ' That\'s my lucky number!';
+            } else {
+                return rando + ' That\'s not my lucky number!';
+            }
+        }
     }
 });
 
