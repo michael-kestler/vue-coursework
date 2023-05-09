76dc7f520abf668b3d4cb524756c7e92c630a016
michael-kestler <michael.s.kestler@gmail.com>
2023-05-09 17:02:08 -0400

    added image to html page with vue

--- basics-assignment-01/app.js
+++ basics-assignment-01/app.js
@@ -4,7 +4,8 @@ const app = Vue.createApp({
             yourName: 'Michael Kestler',
             yourAge: 'Age: 31',
             ageInFive: 'Age in 5 years: 36',
-            randomNumber: 'Favorite Number: ' + (Math.random()).toFixed(2)
+            randomNumber: 'Favorite Number: ' + (Math.random()).toFixed(2),
+            imageUrl: 'https://media.licdn.com/dms/image/C4D03AQGfl52yFeHRow/profile-displayphoto-shrink_800_800/0/1517448638219?e=1689206400&v=beta&t=Hf2plOaVJuNtHRpbbzCpMO-XhV8ry4qh7w0i2nYrru0'
         };
 
     }
