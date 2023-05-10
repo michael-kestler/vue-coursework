f46c22a85adec931a05cdaa6656a864a90653d30
michael-kestler <michael.s.kestler@gmail.com>
2023-05-10 15:48:37 -0400

    pre-populated input field with name using v-model

--- basics-assignment-01/app.js
+++ basics-assignment-01/app.js
@@ -5,7 +5,7 @@ const app = Vue.createApp({
             yourAge: 'Age: 31',
             ageInFive: 'Age in 5 years: 36',
             randomNumber: 'Favorite Number: ' + (Math.random()).toFixed(2),
-            imageUrl: 'https://media.licdn.com/dms/image/C4D03AQGfl52yFeHRow/profile-displayphoto-shrink_800_800/0/1517448638219?e=1689206400&v=beta&t=Hf2plOaVJuNtHRpbbzCpMO-XhV8ry4qh7w0i2nYrru0'
+            imageUrl: 'https://media.licdn.com/dms/image/C4D03AQGfl52yFeHRow/profile-displayphoto-shrink_800_800/0/1517448638219?e=1689206400&v=beta&t=Hf2plOaVJuNtHRpbbzCpMO-XhV8ry4qh7w0i2nYrru0',
         };
     },
     methods: {
