42ed9409175bbe682e18d7a9b0c61db81b51ff3a
michael-kestler <michael.s.kestler@gmail.com>
2023-05-10 16:10:40 -0400

    Changed age in 5 years to add 5 to age rather than hard coding it

--- basics-assignment-01/app.js
+++ basics-assignment-01/app.js
@@ -2,8 +2,8 @@ const app = Vue.createApp({
     data(){
         return{
             yourName: 'Michael Kestler',
-            yourAge: 'Age: 31',
-            ageInFive: 'Age in 5 years: 36',
+            yourAge: 31,
+            // ageInFive: 'Age in 5 years: 36',
             randomNumber: 'Favorite Number: ' + (Math.random()).toFixed(2),
             imageUrl: 'https://media.licdn.com/dms/image/C4D03AQGfl52yFeHRow/profile-displayphoto-shrink_800_800/0/1517448638219?e=1689206400&v=beta&t=Hf2plOaVJuNtHRpbbzCpMO-XhV8ry4qh7w0i2nYrru0',
         };
