955454bc096994222c9bd325d738d15084a3df9a
michael-kestler <michael.s.kestler@gmail.com>
2023-05-11 16:41:03 -0400

    added method to show alert when button is clicked

--- basics-assignments/assignment-02/app.js
+++ basics-assignments/assignment-02/app.js
@@ -1,5 +1,12 @@
 const app = Vue.createApp({
 
+    methods: {
+        submitForm(){
+            alert('Your submission has been recieved.');
+        }
+    }
+
+
 });
 
 app.mount('#assignment');
