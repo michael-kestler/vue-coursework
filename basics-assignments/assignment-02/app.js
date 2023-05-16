ba15f842e56726a06a50af6180f6a142d238ab3b
michael-kestler <michael.s.kestler@gmail.com>
2023-05-16 08:36:29 -0400

    Registered user input on keydown and output in para

--- basics-assignments/assignment-02/app.js
+++ basics-assignments/assignment-02/app.js
@@ -1,11 +1,24 @@
 const app = Vue.createApp({
 
+    data() {
+        return{
+            userInput: ""
+        };
+    },
+
+
     methods: {
         submitForm(){
             alert('Your submission has been recieved.');
+        },
+    
+        setOutput(event){
+            this.userInput = event.target.value;
         }
+    
     }
-
+    
+    
 
 });
 
