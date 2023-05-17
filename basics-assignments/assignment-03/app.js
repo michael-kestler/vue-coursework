4cc94bf7958d0550032f4857e5c7c213d7909b47
michael-kestler <michael.s.kestler@gmail.com>
2023-05-17 17:26:37 -0400

    completed 1st task connect btns and calc a value

---
+++ basics-assignments/assignment-03/app.js
@@ -0,0 +1,20 @@
+
+const app = Vue.createApp({
+    data(){
+        return{
+            counter: 0
+        }
+    },
+
+    methods: {
+
+        add(num){
+            this.counter = this.counter + num;
+        }
+
+    }
+
+
+})
+
+app.mount('#assignment');
