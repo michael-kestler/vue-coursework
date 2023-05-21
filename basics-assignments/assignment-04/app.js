312f77d1c3f99a1e355c67bf47dcc46a52b0122c
Michael Kestler <michael.s.kestler@gmail.com>
2023-05-21 19:15:44 -0400

    used hidden and visbible classes to hide para when button is toggled

--- basics-assignments/assignment-04/app.js
+++ basics-assignments/assignment-04/app.js
@@ -3,22 +3,30 @@ const app = Vue.createApp({
     data(){
         return{
            userInput: '',
-           user1: false,
-           user2: false
-
+           paragraphIsVisible: true
         }
 
     },
 
     methods: {
-       userSelected(userInput){
-            if(userInput === user1){
-                
+        buttonToggled(){
+            this.paragraphIsVisible = !this.paragraphIsVisible;
+        }
+    },
+
+
+    computed: {
+      paraClasses(){
+        return{
+            user1: this.userInput === 'user1',
+            user2: this.userInput === 'user2',
+            visible: this.paragraphIsVisible,
+            hidden: !this.paragraphIsVisible
             }
-       }
-
+        }
     }
-
+                
+  
 });
 
 app.mount('#assignment')
