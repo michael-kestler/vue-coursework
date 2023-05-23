c337af9d074a7dceb43ba5677e43821986941566
michael-kestler <michael.s.kestler@gmail.com>
2023-05-23 17:13:32 -0400

    added code to manage list of tasks in Vue app

--- basics-assignments/assignment-05/app.js
+++ basics-assignments/assignment-05/app.js
@@ -1,5 +1,17 @@
 const app = Vue.createApp ({
 
+    data(){
+        return{
+            enteredTask: '',
+            tasks: []
+        };
+    },
+
+    methods: {
+        addTask(){
+            this.tasks.push(this.enteredTask)
+        }
+    }
 
 
 
