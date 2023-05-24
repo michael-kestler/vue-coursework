7e3b2329e077d935f801640d667b4bc4dd0c4ea9
michael-kestler <michael.s.kestler@gmail.com>
2023-05-24 17:20:16 -0400

    refactored inline css for status bar and put it in a computed property

--- main-folder/projects/monster-slayer-game/app.js
+++ main-folder/projects/monster-slayer-game/app.js
@@ -27,6 +27,18 @@ const app = Vue.createApp({
       this.playerHealth -= attackValue;
     },
   },
+
+  computed:{
+    monsterBarStyles(){
+        return {width: this.monsterHealth + '%'}
+    },
+
+    playerBarStyles(){
+        return {width: this.playerHealth + '%'}
+    }
+  }
+
+
 });
 
 app.mount("#game");
