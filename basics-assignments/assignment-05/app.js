93fa48789393093f902c8cfc3300934d6b89368f
michael-kestler <michael.s.kestler@gmail.com>
2023-05-24 15:43:27 -0400

    reformatted js to clean it up

--- basics-assignments/assignment-05/app.js
+++ basics-assignments/assignment-05/app.js
@@ -1,49 +1,38 @@
-const app = Vue.createApp ({
+const app = Vue.createApp({
+  data() {
+    return {
+      enteredTask: "",
+      tasks: [],
+      listIsVisible: true,
+    };
+  },
 
-    data(){
-        return{
-            enteredTask: '',
-            tasks: [],
-            listIsVisible: true
-        }
+  methods: {
+    addTask() {
+      this.tasks.push(this.enteredTask);
     },
 
-    methods: {
-        addTask(){
-            this.tasks.push(this.enteredTask)
-        },
+    hideShow() {
+      this.listIsVisible = !this.listIsVisible;
+    },
 
-        hideShow(){
-            
-                this.listIsVisible = !this.listIsVisible;
-            },
+    hideShowCaption() {
+      if (this.listIsVisible) {
+        return "Hide";
+      } else {
+        return "Show";
+      }
+    },
+  },
 
-            hideShowCaption(){
-                if (this.listIsVisible){
-                    return "Hide"
-                } else {
-                    return "Show"
-                }
-            }
-           
-            
-        },
-
-        computed: {
-            listClasses(){
-                return {
-                    hidden: !this.listIsVisible,
-                    visible: this.listIVisible
-                }
-
-            }
-
-        }
-
-
-
-
-
+  computed: {
+    listClasses() {
+      return {
+        hidden: !this.listIsVisible,
+        visible: this.listIVisible,
+      };
+    },
+  },
 });
 
-app.mount('#assignment');
+app.mount("#assignment");
