98a05eeed46524f63870819802c7bf053e36916a
michael-kestler <michael.s.kestler@gmail.com>
2023-05-24 16:51:27 -0400

    added js code for player attacking monster and monster attacking player

--- main-folder/projects/monster-slayer-game/app.js
+++ main-folder/projects/monster-slayer-game/app.js
@@ -1,6 +1,31 @@
+//if you don't need access to vue properties you can write regular javascript function
+    function getRandomValue(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+
 const app = Vue.createApp({
     data(){
-        
+        return {
+            playerHealth: 100,
+            monsterHealth: 100
+        }
+    },
+
+    methods: {
+        //when the attack button is clicked we want to trigger a method that reduces the monster's health
+        attackMonster(){
+            const attackValue = getRandomValue(5, 12);
+            // this.monsterHealth = this.monsterHealth - attackValue;
+            this.monsterHealth -= attackValue;
+
+            // when player attacks monster attack player method is triggered
+            this.attackPlayer()
+        },
+
+        attackPlayer() {
+            const attackValue = getRandomValue(8, 15);
+            this.playerHealth -= attackValue;
+        }
     }
 
 });
