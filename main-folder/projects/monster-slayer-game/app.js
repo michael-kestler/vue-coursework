7cc83686746dedbb9e2f91a51d3023bf590718e5
Michael Kestler <michael.s.kestler@gmail.com>
2023-05-27 17:02:35 -0400

    added condition to bar styles to not let bar width go below zero percent

--- main-folder/projects/monster-slayer-game/app.js
+++ main-folder/projects/monster-slayer-game/app.js
@@ -52,10 +52,16 @@ const app = Vue.createApp({
 
   computed: {
     monsterBarStyles() {
+      if (this.monsterHealth < 0){
+        return {width: '0%'}
+      }
       return { width: this.monsterHealth + "%" };
     },
 
     playerBarStyles() {
+      if(this.playerHealth < 0){
+        return {width: '0%'}
+      }
       return { width: this.playerHealth + "%" };
     },
 
